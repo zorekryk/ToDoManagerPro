9f41294d6c44ae5b80165b53163de0fd56071594
Kyryl Kolisnichenko <kolisnichenkokyryl@gmail.com>
2025-11-06 21:58:56 +0200

    refactor(header): wrap h1 with Link, reorganize css with nested selectors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import "./Header.css";
+import { Link } from "react-router";
 import Nav from "../Nav";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <h1>ToDo Manager Pro</h1>
+        <Link to="/">
+          <h1>ToDo Manager Pro</h1>
+        </Link>
         <Nav />
       </div>
     </header>
