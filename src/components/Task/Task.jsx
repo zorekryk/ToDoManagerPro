ca01371156c5b1ea89d23cd33a0d8e8b91ed10dd
Kyryl Kolisnichenko <kolisnichenkokyryl@gmail.com>
2025-11-06 17:28:02 +0200

    Add the ability to delete tasks

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,22 +1,25 @@
 import "./Task.css";
 import Badge from "@/components/shared/Badge";
 import Button from "@/components/shared/Button";
+import { useTasks } from "@/store";
 import { Calendar, SquarePen, Tag, Trash2 } from "lucide-react";
 import { useId } from "react";
 
 const Task = (props) => {
-  const { title, description, category, deadline } = props;
-  const id = useId();
+  const { id, title, description, category, deadline } = props;
+  const fieldId = useId();
+  const removeTask = useTasks((state) => state.removeTask);
+
   return (
     <div className="task card">
       <div className="task-content">
         <input
           className="checkbox"
-          id={id}
+          id={fieldId}
           type="checkbox"
         />
         <div className="task-info">
-          <label htmlFor={id}>{title}</label>
+          <label htmlFor={fieldId}>{title}</label>
           {description && (
             <p>{description}</p>
           )}
@@ -36,7 +39,7 @@ const Task = (props) => {
         <Button classname="ghost">
           <SquarePen />
         </Button>
-        <Button classname="ghost">
+        <Button classname="ghost" onClick={() => removeTask(id)}>
           <Trash2 />
         </Button>
       </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,24 @@
-import { create } from "zustand/react";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useTasks = create((set) => ({
-  tasks: [
-    // {
-    //   id: 1,
-    //   title: "Task 1",
-    //   description: "Task 1 description",
-    //   category: "Life",
-    //   deadline: "10.11.2025"
-    // },
-    // {
-    //   id: 2,
-    //   title: "Task 2",
-    //   description: "Task 2 description",
-    //   category: "Work",
-    //   deadline: "10.11.2025"
-    // },
-    // {
-    //   id: 3,
-    //   title: "Task 3",
-    //   category: "Life",
-    //   deadline: "11.11.2025"
-    // }
-  ],
-  addTask: (task) => set((state) => ({
-    tasks:
-      [...state.tasks,
-        { id: crypto.randomUUID(), ...task }]
-  })),
-  removeTask: () => set()
-}));
+export const useTasks = create(
+  persist(
+    (set) => ({
+      tasks: [],
+
+      addTask: (task) =>
+        set((state) => ({
+          tasks:
+            [...state.tasks,
+              { id: crypto.randomUUID(), ...task }]
+        })),
+
+      removeTask: (id) =>
+        set((state) => ({
+          tasks:
+            state.tasks.filter((task) => task.id !== id)
+        }))
+    }),
+    { name: "tasks" }
+  )
+);
